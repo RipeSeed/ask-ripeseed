24f1f94b93aaf4ffcbc332044cd6a1691ec0bea6
Ahmed Shahram <shahram.ask.546@gmail.com>
2024-05-29 10:31:24 +0500

    feat: add footer component

---
+++ src/components/common/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <div className="p-4 flex justify-center items-center bg-[#3F3D56] text-white h-6">
+      <p>Ⓒ RipeSeed 2024. All Right Reserved</p>
+    </div>
+  );
+};
