2c00920f5d6ce58864e8d4d900ca32d93d04ff03
Ahmed Shahram <shahram.ask.546@gmail.com>
2024-05-30 18:14:32 +0500

    styles: redesign welcome cards

--- src/app/_components/chat/WelcomeCards.tsx
+++ src/app/_components/chat/WelcomeCards.tsx
@@ -26,24 +26,26 @@ export const WelcomeCards = ({ sendMessage }: Props) => {
 
   return (
     <div className="flex justify-center items-center h-full flex-col gap-4">
-      <Note className="w-auto" />
-      <div className="flex justify-center items-center w-[280px] flex-col gap-2">
-        <Card
-          className="bg-primary/20 shadow-[#FFAB00] hover:bg-primary/10 cursor-pointer w-64 h-24 flex justify-center items-center p-2"
-          onClick={() => handleSendMessage(0)}>
-          {cards[0].content}
-        </Card>
-        <div className="flex flex-row gap-2">
+      <div className="max-w-[500px] flex flex-col gap-4">
+        <Note className="w-full" />
+        <div className="flex justify-center items-center flex-col gap-2">
           <Card
-            className="bg-primary/20 shadow-[#FFAB00] hover:bg-primary/10 cursor-pointer w-32 h-24 flex justify-center items-center p-2"
-            onClick={() => handleSendMessage(1)}>
-            {cards[1].content}
+            className="bg-white shadow-none hover:bg-gray-100 cursor-pointer h-24 flex justify-center w-full items-center p-2 text-gray-500"
+            onClick={() => handleSendMessage(0)}>
+            {cards[0].content}
           </Card>
-          <Card
-            className="bg-primary/20 shadow-[#FFAB00] hover:bg-primary/10 cursor-pointer w-32 h-24 flex justify-center items-center p-2"
-            onClick={() => handleSendMessage(2)}>
-            {cards[2].content}
-          </Card>
+          <div className="flex flex-row gap-2 w-full">
+            <Card
+              className="bg-white shadow-none hover:bg-gray-100 cursor-pointer h-24 flex justify-center w-full items-center p-2 text-gray-500"
+              onClick={() => handleSendMessage(1)}>
+              {cards[1].content}
+            </Card>
+            <Card
+              className="bg-white shadow-none hover:bg-gray-100 cursor-pointer h-24 flex justify-center w-full items-center p-2 text-gray-500"
+              onClick={() => handleSendMessage(2)}>
+              {cards[2].content}
+            </Card>
+          </div>
         </div>
       </div>
     </div>
