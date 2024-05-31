6cbd18fa9c3be640649be1c8fbeee006e9bc1588
Ahmed Shahram <shahram.ask.546@gmail.com>
2024-05-31 11:21:46 +0500

    feat: add chats to valtio

--- src/app/_utils/store/index.ts
+++ src/app/_utils/store/index.ts
@@ -2,8 +2,10 @@ import { Chat } from "@/app/_lib/db";
 import { ValtioWrapper } from "./classes";
 
 export const initialState: {
+  chats: Chat[];
   selectedChat: Chat | undefined;
 } = {
+  chats: [],
   selectedChat: {} as Chat | undefined,
 };
 
