d9bea29f453acdc965815295e7e21bebbe893fc8
Ahmed Shahram <ahmed.shahram@ripeseed.io>
2024-06-10 14:31:22 +0500

    add: external link icon on /ask-ripeseed

--- src/app/ask-ripeseed/_components/SidebarView.tsx
+++ src/app/ask-ripeseed/_components/SidebarView.tsx
@@ -2,7 +2,7 @@ import { Chat } from "@/app/_lib/db";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
-import { MessagesSquare } from "lucide-react";
+import { MessagesSquare, ExternalLink as ExternalLinkIcon } from "lucide-react";
 import Link from "next/link";
 
 export const SidebarView = () => {
@@ -79,12 +79,13 @@ const ExternalLink = ({ href, text }: { href: string; text: string }) => {
           variant: "default",
           size: "lg",
         }),
-        `w-full cursor-pointer border-none bg-[#FBFBFB] text-gray-400 shadow-none transition-all hover:bg-[#FBFBFB] hover:text-gray-500 hover:underline`,
+        `w-full cursor-pointer border-none bg-[#FBFBFB] text-gray-400 shadow-none transition-all hover:bg-[#FBFBFB] hover:text-primary hover:underline`,
       )}
     >
       <div className="flex w-full flex-row justify-center">
-        <div className="flex justify-center gap-4 truncate">
-          <span>{text}</span>
+        <div className="flex flex-row items-center justify-center gap-1 truncate">
+          <span>{text} </span>
+          <ExternalLinkIcon className="h-3 w-3" />
         </div>
       </div>
     </Link>
