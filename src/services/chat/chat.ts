1bb24b8316763ef2d8ce200466278f9442533fb8
Ahmed Shahram <ahmed.shahram@ripeseed.io>
2024-06-12 13:05:00 +0500

    update: PDFLoader outdated lib to @langchain/community parser

--- src/services/chat/chat.ts
+++ src/services/chat/chat.ts
@@ -1,23 +1,26 @@
 import type { Document as LangchainDoc } from "langchain/document";
 import { PineconeStore } from "@langchain/pinecone";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { OpenAIEmbeddings } from "@langchain/openai";
 import { pineconeIndex } from "./config";
-
 
 export const splitText = async (file: File, id: string) => {
   try {
     const splitter = new RecursiveCharacterTextSplitter();
     const loader = new PDFLoader(file);
-    const docs = loader.loadAndSplit(splitter)
-    return docs;
+    const laoded = await loader.load();
+    const splittedDocs = await splitter.splitDocuments(laoded);
+    return splittedDocs;
   } catch (error) {
     throw Error("Error splitting text");
   }
 };
 
-export const createEmbeddings = async (docs: LangchainDoc[], embeddings: OpenAIEmbeddings) => {
+export const createEmbeddings = async (
+  docs: LangchainDoc[],
+  embeddings: OpenAIEmbeddings,
+) => {
   try {
     const vectorStore = await PineconeStore.fromDocuments(docs, embeddings, {
       pineconeIndex,
@@ -28,9 +31,13 @@ export const createEmbeddings = async (docs: LangchainDoc[], embeddings: OpenAIE
   }
 };
 
-export const vectorize = async (id: string, file: File, openAIApiKey: string) => {
+export const vectorize = async (
+  id: string,
+  file: File,
+  openAIApiKey: string,
+) => {
   const embeddings = new OpenAIEmbeddings({
-    openAIApiKey
+    openAIApiKey,
   });
 
   let chunks = await splitText(file, id);
