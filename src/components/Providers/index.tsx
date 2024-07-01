792120a19aff79d218e197d93536cf22689943b4
Ahmed Shahram <ahmed.shahram@ripeseed.io>
2024-07-01 11:05:39 +0500

    feature: add dark theme provider.

---
+++ src/components/Providers/index.tsx
@@ -0,0 +1,21 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { ThemeProvider } from "@/components/Providers/ThemeProvider";
+
+const queryClient = new QueryClient();
+
+export default function Providers({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <QueryClientProvider client={queryClient}>
+          <TooltipProvider>{children}</TooltipProvider>
+        </QueryClientProvider>
+      </ThemeProvider>
+    </>
+  );
+}
