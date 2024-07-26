7c5bf783ba1f0226d70020fcec680fd069c985d4
Afaq Ahmad <afaq.ahmad@ripeseed.io>
2024-07-26 16:46:24 +0500

    added parallel routing for sidebar and chat panel

--- src/app/general/page.tsx
+++ src/app/general/page.tsx
@@ -1,23 +1,6 @@
 "use client";
-import { useRouter } from "next/navigation";
-import { getAllChats } from "@/app/_lib/db";
-import { useEffect } from "react";
-import { Chat } from "./[chatId]/_components/Chat";
+import { ChatMessages } from "./[chatId]/_components/ChatMessages";
 
 export default function Page() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const getChats = async () => {
-      const _chats = await getAllChats();
-      if (_chats.length) {
-        router.push(`/general/${_chats[_chats.length - 1].id}`);
-      }
-    };
-
-    void getChats();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return <Chat />;
+  return <ChatMessages />;
 }
