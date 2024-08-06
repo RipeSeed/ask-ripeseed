0ffcf1008419b0b3c0442ab3930347440b41b13f
Usman-Ahmad673 <itsusmanahmad00@gmail.com>
2024-08-06 11:49:35 +0500

    new component of sidebar for mobile view

--- src/app/general/[chatId]/page.tsx
+++ src/app/general/[chatId]/page.tsx
@@ -1,17 +1,10 @@
 "use client";
 import { useState } from "react";
 import { ChatMessages } from "./_components/ChatMessages";
-import { Menu, X } from "lucide-react";
-import GeneralSideBar from "@/app/@sidebar/_components/GeneralSidebar";
-
+import MobileSideBar from "@/components/common/_components/MobileSideBar";
 
 const Page = () => {
   const [toggle, setToggle] = useState(false);
-
-  const togglehandler = (e: React.MouseEvent) => {
-    e.stopPropagation();
-    setToggle(!toggle);
-  };  
 
   return (
     <div
@@ -20,27 +13,12 @@ const Page = () => {
     >
       <div className="z-10 h-full w-full text-sm lg:flex">
         <div className="flex h-[calc(100svh-57px)] w-full flex-col justify-between md:h-[calc(100svh-93px)]">
-          {toggle ? (
-            <X
-              onClick={(e) => togglehandler(e)}
-              className="absolute left-[14px] top-4 h-5 w-5 cursor-pointer md:hidden"
-            />
-          ) : (
-            <Menu
-              className="absolute left-[14px] top-4 h-5 w-5 cursor-pointer md:hidden"
-              onClick={(e) => togglehandler(e)}
-            />
-          )}
-          <div
-            className={`absolute top-12 mt-3 z-40 flex h-full w-[200px] transform flex-col bg-[rgb(235,235,235)] p-4 transition-transform duration-200 ease-in-out dark:bg-black md:hidden ${toggle ? "translate-x-0" : "-translate-x-full"}`}
-            onClick={(event) => event.stopPropagation()}
-          >
-            <GeneralSideBar />
-          </div>
+          <MobileSideBar toggle={toggle} setToggle={setToggle} />
           <ChatMessages />
         </div>
       </div>
     </div>
   );
 };
+
 export default Page;
