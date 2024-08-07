d6a5dde01753562c20c1e12b70af8b5460b37a1b
Afaq Ahmad <afaq.ahmad@ripeseed.io>
2024-08-07 16:02:19 +0500

    added delete functionality for RS chat

--- src/components/common/_components/ChatButtonsHeader.tsx
+++ src/components/common/_components/ChatButtonsHeader.tsx
@@ -13,9 +13,12 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import {
+  deleteAllMessages_aRS
+} from "@/app/_lib/db";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { ExternalLink } from "lucide-react";
+import { ExternalLink, Trash } from "lucide-react";
 import { toast } from "sonner";
 
 import { Settings } from "lucide-react";
@@ -31,28 +34,104 @@ export default function ChatHeader() {
 
   return (
     <div className="sticky top-0 flex items-center justify-center border-b border-[#ACACAC] bg-[#E8E8E8] py-3 dark:border-[#1B1B21] dark:bg-[#363639] md:py-6">
-    <div className="flex gap-1 rounded-full bg-[#E0E0E0] text-[#575757] dark:bg-[#5E5E61] dark:text-white">
-      <li
-        onClick={() => router.push("/ask-ripeseed")}
-        className={`cursor-pointer list-none px-2 py-1 font-medium transition-all duration-300 ease-linear xs:px-6 md:py-2 md:text-lg ${isPath(askRSPaths, pathname) ? "rounded-full bg-crayola text-white drop-shadow-lg" : "rounded-full hover:bg-accent"} `}
-      >
-        Ask RipeSeed
-      </li>
-      <li
-        onClick={() => router.push("/general")}
-        className={`cursor-pointer list-none px-2 py-1 font-medium transition-all duration-300 ease-linear xs:px-6 md:py-2 md:text-lg ${isPath(generalPaths, pathname) ? "rounded-full bg-crayola text-white drop-shadow-lg" : "rounded-full hover:bg-accent"}`}
-      >
-        Ask Anything
-      </li>
+      <div className="flex gap-1 rounded-full bg-[#E0E0E0] text-[#575757] dark:bg-[#5E5E61] dark:text-white">
+        <li
+          onClick={() => router.push("/ask-ripeseed")}
+          className={`cursor-pointer list-none px-2 py-1 font-medium transition-all duration-300 ease-linear xs:px-6 md:py-2 md:text-lg ${isPath(askRSPaths, pathname) ? "rounded-full bg-crayola text-white drop-shadow-lg" : "rounded-full hover:bg-accent"} `}
+        >
+          Ask RipeSeed
+        </li>
+        <li
+          onClick={() => router.push("/general")}
+          className={`cursor-pointer list-none px-2 py-1 font-medium transition-all duration-300 ease-linear xs:px-6 md:py-2 md:text-lg ${isPath(generalPaths, pathname) ? "rounded-full bg-crayola text-white drop-shadow-lg" : "rounded-full hover:bg-accent"}`}
+        >
+          Ask Anything
+        </li>
+      </div>
+      <div className="absolute right-0 mr-[14px]">
+        {isPath(askRSPaths, pathname) ? (
+          <DeleteConfirmationDialog />
+        ) : (
+          <ConfigDialogue />
+        )}
+      </div>
     </div>
-    <div className="absolute right-0 mr-[14px]">
-      <ConfigDialogue hidden={!isPath(configPaths, pathname)}/>
-    </div>
-  </div>
   );
 }
 
-const ConfigDialogue = ({ hidden = false } : { hidden?: boolean }) => {
+const DeleteConfirmationDialog = () => {
+  const closeRef = useRef<HTMLButtonElement>(null);
+  const openRef = useRef<HTMLButtonElement>(null);
+  const { set, useSnapshot } = store;
+  const { isDeleteDialogOpen } = useSnapshot();
+
+  useEffect(() => {
+    if (isDeleteDialogOpen) {
+      openRef.current?.click();
+    }
+    const closeBtn = closeRef.current;
+    return () => {
+      set("isDeleteDialogOpen", false);
+      closeBtn?.click();
+    };
+  }, [isDeleteDialogOpen, set]);
+
+  const confirmDelete = async () => {
+    await deleteAllMessages_aRS();
+    set("clearChat", true);
+    closeRef.current?.click();
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild className="flex items-center justify-center">
+        <Button
+          className={"border-none bg-transparent shadow-none hover:shadow-sm"}
+          ref={openRef}
+          variant="outline"
+          size="icon"
+        >
+          <Trash className="h-5 w-5 cursor-pointer rounded-xl stroke-[#1B1B21] text-muted-foreground dark:stroke-[#EBEBEB]" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="rounded-lg sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle>Delete Confirmation</DialogTitle>
+        </DialogHeader>
+        <div className="flex items-center space-x-2">
+          <div className="grid flex-1 gap-2">
+            <Label>
+              Are you sure you want to delete all the chat and start over?
+            </Label>
+          </div>
+        </div>
+        <DialogFooter className="w-full flex-col space-y-1">
+          <div className="flex flex-col gap-2 sm:flex-row sm:justify-end">
+            <DialogClose asChild>
+              <Button
+                type="button"
+                variant="secondary"
+                className="rounded-full"
+                ref={closeRef}
+              >
+                No
+              </Button>
+            </DialogClose>
+            <Button
+              type="button"
+              onClick={confirmDelete}
+              className="rounded-full bg-crayola"
+            >
+              Yes
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+const ConfigDialogue = () => {
   const closeRef = useRef<HTMLButtonElement>(null);
   const openRef = useRef<HTMLButtonElement>(null);
   const { set, useSnapshot } = store;
@@ -104,7 +183,7 @@ const ConfigDialogue = ({ hidden = false } : { hidden?: boolean }) => {
     <Dialog>
       <DialogTrigger asChild className="flex items-center justify-center">
         <Button
-          className={`border-none bg-transparent shadow-none hover:shadow-sm ${hidden ? "invisible" : ""}`}
+          className={"border-none bg-transparent shadow-none hover:shadow-sm"}
           ref={openRef}
           variant="outline"
           size="icon"
