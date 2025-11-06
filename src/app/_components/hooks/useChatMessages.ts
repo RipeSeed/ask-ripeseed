92659ce0adc1bd3ff6b3f265ddef47d6c52bdb9d
riazosama <osama.riaz@ripeseed.io>
2025-11-06 12:23:10 +0500

    refactor: remove unused useState import in useChatMessages hook

--- src/app/_components/hooks/useChatMessages.ts
+++ src/app/_components/hooks/useChatMessages.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useChat } from '@ai-sdk/react'
 import { createId } from '@paralleldrive/cuid2'
 import { useQuery, useQueryClient } from '@tanstack/react-query'
